8c646662a39304bb76d19673e789b1d2c172461c
Scar26 <mmatty26@gmail.com>
2022-03-25 04:47:56 +0530

    test
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- src/models/setup.go
+++ src/models/setup.go
@@ -13,6 +13,7 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	fmt.Println(dsn)
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
